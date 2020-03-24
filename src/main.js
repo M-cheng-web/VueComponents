5effac3246d0a6ef928d783a262c0d85bcef0dd5
M-cheng-web <2604856589@qq.com>
2020-03-24 23:19:55 +0800

    加了一些样式mixin

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import store from './store'
 import SvgIcon from './components/SvgIcon' // 阿里 svg图片展示
 
 // ------------------------------------------------------ 引入资源
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import '@/assets/svg' // svg图
 
 // ------------------------------------------------------ 引入模块
@@ -15,8 +17,10 @@ import apis from '@/api' // api
 // ------------------------------------------------------ 全局挂载模块
 Vue.prototype.$api = apis
 
-// ------------------------------------------------------ 注册全剧组件
+// ------------------------------------------------------ 注册全局组件
 Vue.component('svg-icon', SvgIcon)
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
