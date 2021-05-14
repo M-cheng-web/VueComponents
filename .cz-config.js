b11b55fb9f241c8a2c40b96d0cef66a3896d02fe
M-cheng-web <2604856589@qq.com>
2021-05-14 13:37:51 +0800

    docs(dddd): zzzzz
    ccccc
    
    ISSUES CLOSED: eeeee

--- .cz-config.js
+++ .cz-config.js
@@ -1,44 +1,34 @@
 'use strict';
 module.exports = {
-  // 项目中使用的 type 和默认描述
   types: [
-    { value: '✨特性', name: '特性:    一个新的特性' },
-    { value: '🐛修复', name: '修复:    修复一个Bug' },
-    { value: '📝文档', name: '文档:    变更的只有文档' },
-    { value: '💄格式', name: '格式:    空格, 分号等格式修复' },
-    { value: '♻️重构', name: '重构:    代码重构，注意和特性、修复区分开' },
-    { value: '⚡️性能', name: '性能:    提升性能' },
-    { value: '✅测试', name: '测试:    添加一个测试' },
-    { value: '🔧工具', name: '工具:    开发工具变动(构建、脚手架工具等)' },
-    { value: '⏪回滚', name: '回滚:    代码回退' }
+    { value: 'init', name: 'init:     初始提交' },
+    { value: 'feat', name: 'feat:     增加新功能' },
+    { value: 'fix', name: 'fix:      修复bug' },
+    { value: 'ui', name: 'ui:       更新UI' },
+    { value: 'refactor', name: 'refactor: 代码重构' },
+    { value: 'release', name: 'release:  发布' },
+    { value: 'deploy', name: 'deploy:   部署' },
+    { value: 'docs', name: 'docs:     修改文档' },
+    { value: 'test', name: 'test:     增删测试' },
+    { value: 'chore', name: 'chore:    更改配置文件' },
+    { value: 'style', name: 'style:    样式修改不影响逻辑' },
+    { value: 'revert', name: 'revert:   版本回退' },
+    { value: 'add', name: 'add:      添加依赖' },
+    { value: 'minus', name: 'minus:    版本回退' },
+    { value: 'del', name: 'del:      删除代码/文件' }
   ],
-  // 预设项目中使用的可选 scope 
-  scopes: [
-    { name: '模块1' },
-    { name: '模块2' },
-    { name: '模块3' },
-    { name: '模块4' }
-  ],
-  // 当想重写特定提交类型的作用域时，使用此方法 如：在类型为“fix”时指定范围
-  // scopeOverrides: {
-  //   fix: [
-  //     { name: 'merge' },
-  //     { name: 'style' },
-  //     { name: 'e2eTest' },
-  //     { name: 'unitTest' }
-  //   ]
-  // },
+  scopes: [],
   messages: {
     type: '选择更改类型:\n',
-    scope: '更改范围 (可选):\n',
-    customScope: 'Denote the SCOPE of this change:',
+    scope: '更改的范围:\n',
+    // 如果allowcustomscopes为true，则使用
+    // customScope: 'Denote the SCOPE of this change:',
     subject: '简短描述:\n',
-    body: '详细描述,使用"|"换行(可选)：\n',
-    breaking: '非兼容性说明(可选):\n',
-    footer: '关联关闭的issue,例如：#31, #34(可选):\n',
-    confirmCommit: '确定提交?'
+    body: '详细描述. 使用"|"换行:\n',
+    breaking: 'Breaking Changes列表:\n',
+    footer: '关闭的issues列表. E.g.: #31, #34:\n',
+    confirmCommit: '确认提交?'
   },
-  allowCustomScopes: true, // 增加自定义 scope 选项
-  // allowBreakingChanges: ['特性', '修复'], // 配置想要 breaking change 弹出提示的scope列表
-  subjectLimit: 100 // 限制主题长度
+  allowCustomScopes: true,
+  allowBreakingChanges: ["feat", "fix"]
 };
